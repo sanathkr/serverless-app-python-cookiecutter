474204fde353281c687694a3a568c285a9cfe8f8
Sanath Kumar Ramesh <sanathkr@amazon.com>
2019-04-25 10:42:29 -0700

    Py37 only

--- {{cookiecutter.service_name}}/setup.py
+++ {{cookiecutter.service_name}}/setup.py
@@ -36,7 +36,7 @@ setup(
     packages=find_packages(exclude=['tests.*', 'tests']),
     keywords="{{cookiecutter.service_name}} Service",
     # Support Python 2.7 and 3.6 or greater
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*',
+    python_requires='>=3.7.*',
     include_package_data=True,
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -47,8 +47,6 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Topic :: Internet',
         'Topic :: Software Development :: Build Tools',
